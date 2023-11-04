456666b5a60efafdb36b8b0687878dcc7c3393e8
suhaib <suhaibvbm@gmail.com>
2023-11-04 10:36:28 +0530

    Highscore never clear after exit (file data store method)

--- scoreborad.py
+++ scoreborad.py
@@ -12,7 +12,8 @@ class ScoreBoard(Turtle):
         self.hideturtle()
         self.goto(0, 260)
         self.score = 0
-        self.highscore = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.update_scoreboard()
 
     def update_scoreboard(self):
@@ -26,9 +27,13 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt", mode= "w") as data:
+                data.write(str(self.highscore))
+
         self.score = 0
         self.update_scoreboard()
     # def game_over(self):
     #            self.goto(0, 0)
     #            self.write("Game over", align="center", font=FONT)
+
 
